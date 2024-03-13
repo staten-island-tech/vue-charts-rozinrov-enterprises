382a1148e7fa7aa40a9ad96be52b72ca228b3b70
Nosha08 <noah.rozin@outlook.com>
2024-03-12 22:36:22 -0400

    a

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,4 +1,6 @@
 <script setup>
+const funds = '100000'
+
 async function fetchData() {
   const key = 'ed8fa9c810584467a3aee5573443fb41'
   const api = `https://api.twelvedata.com/time_series?symbol=AAPL&interval=1min&apikey=${key}`
@@ -13,9 +15,9 @@ fetchData()
 <template>
   <main>
     <h1>Rozinrov</h1>
+    <h2></h2>
   </main>
 </template>
 
 <style scoped>
-
 </style>

c9a5f7acd2271d309e8bd6d224fa78f932e0ffcc
gliberov <124058342+gliberov@users.noreply.github.com>
2024-03-24 23:23:38 -0400

    yes yes yes yes yes yes

---
+++ vite-project/src/components/PiePortfolio.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+    <Pie :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+  
+  <script>
+  import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+  import { Pie } from 'vue-chartjs'
+  
+  ChartJS.register(ArcElement, Tooltip, Legend)
+  export default {
+  name: 'PieChart',
+  components: { Pie },
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    chartOptions: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+    width: 2000px;
+    height: 600px;
+    margin: auto;
+    justify-content: center;
+    display: flex;
+}
+</style>
--- vite-project/src/views/Data2View.vue
+++ vite-project/src/views/Data2View.vue
@@ -1,7 +1,53 @@
 <template>
-  <h1>Data Visualization Center #2</h1>
+  <div>
+    <h1>Data Visualization Center #2</h1>
+    <PieChart :chartData="portfolioData" />
+  </div>
 </template>
 
-<style>
+<script>
+import PieChart from '@/components/PiePortfolio.vue'
+import { computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
 
-</style>
+export default {
+  name: 'PortfolioView',
+  components: { PieChart },
+  setup() {
+    const store = useStore()
+
+    
+    const history = computed(() => store.state.history)
+
+    
+    const portfolioData = computed(() => {
+      const portfolio = {}
+
+      
+      history.value.forEach(item => {
+        if (portfolio[item.id]) {
+          portfolio[item.id] += item.quantity
+        } else {
+          portfolio[item.id] = item.quantity
+        }
+      })
+
+    
+      const labels = Object.keys(portfolio)
+      const datasets = [{
+        data: Object.values(portfolio),
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0']
+      }]
+
+      return { labels, datasets }
+    })
+
+  
+    onMounted(() => {
+      store.dispatch('loadHistory') 
+    })
+
+    return { portfolioData }
+  }
+}
+</script>
